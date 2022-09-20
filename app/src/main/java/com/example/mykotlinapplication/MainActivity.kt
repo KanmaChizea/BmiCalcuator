3d4d4fdd04310f2cf7c7573492b8993f20b07e5a
Kanma Chizea <hchizea@gmail.com>
2022-09-21 00:37:21 +0100

    result

--- app/src/main/java/com/example/mykotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapplication/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.mykotlinapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var ageEditText : EditText
+    private lateinit var weightEditText : EditText
+    private lateinit var heightEditText : EditText
+    private lateinit var age:String
+    private lateinit var weight:String
+    private lateinit var height:String
+    private var gender = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,10 +23,12 @@ class MainActivity : AppCompatActivity() {
         val maleButton = findViewById<ImageButton>(R.id.btnMale)
         val femaleButton = findViewById<ImageButton>(R.id.btnFemale)
         maleButton.setOnClickListener {
+            gender = true
             maleButton.background = ContextCompat.getDrawable(this,R.drawable.border)
             femaleButton.setBackgroundResource(0)
         }
         femaleButton.setOnClickListener {
+            gender = true
             femaleButton.background = ContextCompat.getDrawable(this,R.drawable.border)
             maleButton.setBackgroundResource(0)
         }
@@ -29,17 +36,89 @@ class MainActivity : AppCompatActivity() {
         //calculate
         val calculateButton = findViewById<Button>(R.id.btnCalculate)
         calculateButton.setOnClickListener {
-            val ageTextView = findViewById<EditText>(R.id.etAge)
-            val weightTextView = findViewById<EditText>(R.id.etWeight)
-            val heightTextView = findViewById<EditText>(R.id.etHeight)
-            val age = ageTextView.text.toString()
-            val weight = weightTextView.text.toString()
-            val height = heightTextView.text.toString()
-            val bmi = weight.toFloat()/((height.toFloat() * height.toFloat()) / 10000)
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("AGE", age)
+             ageEditText = findViewById(R.id.etAge)
+            weightEditText = findViewById(R.id.etWeight)
+            heightEditText = findViewById(R.id.etHeight)
+             age = ageEditText.text.toString()
+            weight = weightEditText.text.toString()
+            height = heightEditText.text.toString()
+            if(validate()) {
+                val bmi = weight.toFloat() / ((height.toFloat() * height.toFloat()) / 10000)
+                val bundle = Bundle()
+                bundle.putString("AGE", age)
+                bundle.putString("WEIGHT", weight)
+                bundle.putString("HEIGHT", height)
+                bundle.putString("BMI", bmi.toString())
+               val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent, bundle)
+            }
 
         }
 
+    }
+
+
+    private fun validate() : Boolean {
+
+        val errorAge = findViewById<TextView>(R.id.errorAge)
+        val errorHeight = findViewById<TextView>(R.id.errorHeight)
+        val errorWeight = findViewById<TextView>(R.id.errorWeight)
+        val fields = arrayListOf(true, true, true,gender)
+
+        //handle gender
+        if(!gender){
+            Toast.makeText(this,"Select your gender", Toast.LENGTH_SHORT).show()
+        }
+
+        //handle age
+        if (age == "") {
+            setErrorState(errorAge, ageEditText)
+            fields[0] = false
+        } else {
+            fields[0] = true
+            clearErrorState(errorAge,ageEditText)
+        }
+
+        //handle height
+        if (height == "") {
+            setErrorState(errorHeight, heightEditText)
+            fields[1] = false
+        } else {
+            fields[1] = true
+            clearErrorState(errorHeight,heightEditText)
+        }
+
+        //handle weight
+        if (weight == "") {
+            setErrorState(errorWeight, weightEditText)
+            fields[2] = false
+        } else {
+            fields[2] = true
+            clearErrorState(errorWeight,weightEditText)
+        }
+      return  when(fields.contains(false)){
+            true -> false
+            false -> true
+        }
+
+
+    }
+
+    private fun setErrorState(tv: TextView, et:EditText){
+        val errorText = "Field cannot be empty"
+        DrawableCompat.setTint(
+            et.background,
+            ContextCompat.getColor(this, R.color.error)
+        )
+        tv.text = errorText
+    }
+
+    private fun clearErrorState(tv: TextView, et:EditText){
+        DrawableCompat.setTint(
+            et.background,
+            ContextCompat.getColor(this, R.color.white)
+        )
+        tv.text = ""
     }
 }
--- app/src/main/java/com/example/mykotlinapplication/ResultActivity.kt
+++ app/src/main/java/com/example/mykotlinapplication/ResultActivity.kt
@@ -1,11 +1,56 @@
 package com.example.mykotlinapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlin.math.log
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+         lateinit var age: String
+        lateinit var height: String
+        lateinit var weight: String
+        lateinit var bmi: String
+        val bundle = intent.extras
+bundle?.let {
+                age = bundle.getString("AGE") ?: "0"
+                weight = bundle.getString("WEIGHT") ?: "0"
+                 height = bundle.getString("HEIGHT") ?: "0"
+                 bmi = bundle.getString("BMI") ?: "0"
+            }
+            val ageTextView = findViewById<TextView>(R.id.tvAgeValue)
+            val weightTextView = findViewById<TextView>(R.id.tvWeightValue)
+            val heightTextView = findViewById<TextView>(R.id.tvHeightValue)
+            val bmiTextView = findViewById<TextView>(R.id.tvIndex)
+            val descriptionTextView = findViewById<TextView>(R.id.tvDescription)
+
+            ageTextView.text = age
+            weightTextView.text = weight
+            heightTextView.text = height
+            bmiTextView.text = String.format("%.2f",bmi.toFloat())
+            descriptionTextView.text = getDescription(bmi!!.toFloat())
+
+
+        val retryButton = findViewById<Button>(R.id.btnRetry)
+        retryButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+
+
+    private fun getDescription(bmi:Float):String{
+       return when{
+            bmi < 18.5 -> "Underweight"
+           bmi in 18.5..24.9 -> "Normal weight"
+           bmi in 25.0..29.9 -> "Overweight"
+           else -> "Obese"
+        }
     }
 }
